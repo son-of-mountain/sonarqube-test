6ad04275a1fa0aaf40bfeea3d10eba31f9744d7a
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-11-20 23:28:22 +0100

    first commit

---
+++ src/main/java/com/example/applicationsimple/ApplicationSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicationsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/applicationsimple/ApplicationSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
